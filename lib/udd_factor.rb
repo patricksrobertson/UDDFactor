4839aba7384d74823d64c80dbd14dc00f1ec0c14
jbcplatter <patricksrobertson@gmail.com>
2009-09-05 14:52:17 -0400

    add output type

--- lib/udd_factor.rb
+++ lib/udd_factor.rb
@@ -106,70 +106,73 @@ return (mort_disc * payment) / ((1.0 + interest) ** (numberOfMonths / 12.0))
   module_function :unit_payment
   
   def generate_factor(immediateAge=0.0,commencementAge=65.0,spAge=0.0,
-jsType=0.0,jsPct=0.0,mortality=MortalityTable::PPA2009,
-intSegmentA=5.0,intSegmentB=0.0,intSegmentC=0.0,certainPeriod=0.0,
-tempPeriod=0.0,rounding=12.0)
-errors = []
-immediateCalculation = 0.0
-jointCalculation = 0.0
+	jsType=0.0,jsPct=0.0,mortality=MortalityTable::PPA2009,
+	intSegmentA=5.0,intSegmentB=0.0,intSegmentC=0.0,certainPeriod=0.0,
+	tempPeriod=0.0,rounding=12.0,outputType=0.0)
+	errors = []
+	immediateCalculation = 0.0
+	jointCalculation = 0.0
  
-#set default blank input items
-immediateAge = GenFactor::set_default(immediateAge,commencementAge)
-spAge = GenFactor::set_default(spAge,0.0)
-jsType = GenFactor::set_default(jsType,0.0)
-jsPct = GenFactor::set_default(jsPct,0.0)
-mortality = GenFactor::set_default(mortality,MortalityTable::PPA2009)
-intSegmentB = GenFactor::set_default(intSegmentB,intSegmentA)
-intSegmentC = GenFactor::set_default(intSegmentC,intSegmentA)
-certainPeriod = GenFactor::set_default(certainPeriod,0.0)
-tempPeriod = GenFactor::set_default(tempPeriod,0.0)
-rounding = GenFactor::set_default(rounding,12.0)
+	#set default blank input items
+	immediateAge = GenFactor::set_default(immediateAge,commencementAge)
+	spAge = GenFactor::set_default(spAge,0.0)
+	jsType = GenFactor::set_default(jsType,0.0)
+	jsPct = GenFactor::set_default(jsPct,0.0)
+	mortality = GenFactor::set_default(mortality,MortalityTable::PPA2009)
+	intSegmentB = GenFactor::set_default(intSegmentB,intSegmentA)
+	intSegmentC = GenFactor::set_default(intSegmentC,intSegmentA)
+	certainPeriod = GenFactor::set_default(certainPeriod,0.0)
+	tempPeriod = GenFactor::set_default(tempPeriod,0.0)
+	rounding = GenFactor::set_default(rounding,12.0)
  
-#check to make sure we can do the calculation first
-GenFactor::validate_float(immediateAge) != nil ? errors << "Immediate Age: " + GenFactor::validate_float(immediateAge) :
-GenFactor::validate_float(commencementAge) != nil ? errors << "Commencement Age: " + GenFactor::validate_float(commencementAge) :
-GenFactor::validate_float(intSegmentA) != nil ? errors << "Interest Segment A: " + GenFactor::validate_float(intSegmentA) :
-GenFactor::validate_float(spAge) != nil ? errors << "Spousal Age: " + GenFactor::validate_float(spAge) :
-GenFactor::validate_float(jsType) != nil ? errors << "Joint Factor Type: " + GenFactor::validate_float(jsType) :
-GenFactor::validate_float(jsPct) != nil ? errors << "Joint Factor Percent: " + GenFactor::validate_float(jsPct) :
-#errors << validate_mortality(mortality) #not implemented yet
-GenFactor::validate_float(intSegmentB) != nil ? errors << "Interest Segment B: " + GenFactor::validate_float(intSegmentB) :
-GenFactor::validate_float(intSegmentC) != nil ? errors << "Interest Segment C: " + GenFactor::validate_float(intSegmentC) :
-GenFactor::validate_float(certainPeriod) != nil ? errors << "Certain Period: " + GenFactor::validate_float(certainPeriod) :
-GenFactor::validate_float(tempPeriod) != nil ? errors << "Temporary Period: " + GenFactor::validate_float(tempPeriod) :
-GenFactor::validate_float(rounding) != nil ? errors << "Rounding: " + GenFactor::validate_float(rounding) :
+	#check to make sure we can do the calculation first
+	GenFactor::validate_float(immediateAge) != nil ? errors << "Immediate Age: " + GenFactor::validate_float(immediateAge) :
+	GenFactor::validate_float(commencementAge) != nil ? errors << "Commencement Age: " + GenFactor::validate_float(commencementAge) :
+	GenFactor::validate_float(intSegmentA) != nil ? errors << "Interest Segment A: " + GenFactor::validate_float(intSegmentA) :
+	GenFactor::validate_float(spAge) != nil ? errors << "Spousal Age: " + GenFactor::validate_float(spAge) :
+	GenFactor::validate_float(jsType) != nil ? errors << "Joint Factor Type: " + GenFactor::validate_float(jsType) :
+	GenFactor::validate_float(jsPct) != nil ? errors << "Joint Factor Percent: " + GenFactor::validate_float(jsPct) :
+	#errors << validate_mortality(mortality) #not implemented yet
+	GenFactor::validate_float(intSegmentB) != nil ? errors << "Interest Segment B: " + GenFactor::validate_float(intSegmentB) :
+	GenFactor::validate_float(intSegmentC) != nil ? errors << "Interest Segment C: " + GenFactor::validate_float(intSegmentC) :
+	GenFactor::validate_float(certainPeriod) != nil ? errors << "Certain Period: " + GenFactor::validate_float(certainPeriod) :
+	GenFactor::validate_float(tempPeriod) != nil ? errors << "Temporary Period: " + GenFactor::validate_float(tempPeriod) :
+	GenFactor::validate_float(rounding) != nil ? errors << "Rounding: " + GenFactor::validate_float(rounding) :
  
-if errors.empty?
-#agjust ages/periods to 1/12th of a year, make sure interest rates are grrrrreat!
-immediateAge = GenFactor::sanitize_age(Float(immediateAge))
-commencementAge = GenFactor::sanitize_age(Float(commencementAge))
-spAge = GenFactor::sanitize_age(Float(spAge))
-jsType = GenFactor::sanitize_js_type(Float(jsType))
-jsPct = GenFactor::sanitize_js_pct(Float(jsPct))
-intSegmentA = GenFactor::sanitize_interest(Float(intSegmentA))
-intSegmentB = GenFactor::sanitize_interest(Float(intSegmentB))
-intSegmentC = GenFactor::sanitize_interest(Float(intSegmentC))
-certainPeriod = GenFactor::sanitize_age(Float(certainPeriod))
-tempPeriod = GenFactor::sanitize_age(Float(tempPeriod))
-rounding = Float(rounding)
-end
-if errors.empty?
-#now check for logical errors
-if (immediateAge < commencementAge) && (tempPeriod > 0.0)
-errors << "Error: Deferred Calculation with a Temporary Period"
-end
-if (immediateAge > commencementAge)
-errors << "Error: Commencement age must be greater than or equal to immediate age"
-end
-end
-if errors.empty?
-#return [immediateAge,commencementAge, spAge, intSegmentA, intSegmentB, intSegmentC, certainPeriod,tempPeriod,rounding]
-return calculate_present_value(immediateAge,commencementAge,spAge,jsType,jsPct,
-intSegmentA,intSegmentB,intSegmentC,certainPeriod,
-tempPeriod,rounding)
-else
-return errors
-end
+	if errors.empty?
+		#agjust ages/periods to 1/12th of a year, make sure interest rates are grrrrreat!
+		immediateAge = GenFactor::sanitize_age(Float(immediateAge))
+		commencementAge = GenFactor::sanitize_age(Float(commencementAge))
+		spAge = GenFactor::sanitize_age(Float(spAge))
+		jsType = GenFactor::sanitize_js_type(Float(jsType))
+		jsPct = GenFactor::sanitize_js_pct(Float(jsPct))
+		intSegmentA = GenFactor::sanitize_interest(Float(intSegmentA))
+		intSegmentB = GenFactor::sanitize_interest(Float(intSegmentB))
+		intSegmentC = GenFactor::sanitize_interest(Float(intSegmentC))
+		certainPeriod = GenFactor::sanitize_age(Float(certainPeriod))
+		tempPeriod = GenFactor::sanitize_age(Float(tempPeriod))
+		rounding = Float(rounding)
+	end
+	if errors.empty?
+		#now check for logical errors
+		if (immediateAge < commencementAge) && (tempPeriod > 0.0)
+			errors << "Error: Deferred Calculation with a Temporary Period"
+		end
+		if (immediateAge > commencementAge)
+			errors << "Error: Commencement age must be greater than or equal to immediate age"
+		end
+	end
+	if errors.empty?
+		if 0 == outputType
+			return calculate_present_value(immediateAge,commencementAge,spAge,jsType,jsPct,
+											intSegmentA,intSegmentB,intSegmentC,certainPeriod,
+											tempPeriod,rounding)
+		else
+			return true
+		end
+	else
+		return errors
+	end
   end
   module_function :generate_factor
   
