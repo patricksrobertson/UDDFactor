59440f789e0c2e43ac10e0183d183f4dd412dbe0
jbcplatter <patricksrobertson@gmail.com>
2010-01-12 16:52:14 -0500

    add ActuarialFactor to init.rb

--- init.rb
+++ init.rb
@@ -1,5 +1,5 @@
 # Include hook code here
-require "actuarial_factor"
+require 'actuarial_factor'
 ActionController::Base.send :include, UDDFactor
 ActionController::Base.send :include, AnnuityCertain
 #ActionController::Base.send :include, ActuarialFactor
